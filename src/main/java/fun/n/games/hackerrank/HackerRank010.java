c18c5dcfbf686b3a730eb9382abccc0560dc3682
kaunjovi <kaunjovi@gmail.com>
2018-04-18 00:31:01 +0530

    continuing with hackerrank.

---
+++ src/main/java/fun/n/games/hackerrank/HackerRank010.java
@@ -0,0 +1,22 @@
+package fun.n.games.hackerrank;
+
+import java.util.Scanner;
+
+public class HackerRank010 {
+
+	// https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int one = input.nextInt();
+		int two = input.nextInt();
+		int three = input.nextInt();
+		input.close();
+
+		System.out.println(one);
+		System.out.println(two);
+		System.out.println(three);
+
+	}
+}
+
+// solved at one attempt. sad that i have to try.
---
+++ src/main/java/fun/n/games/hackerrank/HackerRank015.java
@@ -0,0 +1,18 @@
+package fun.n.games.hackerrank;
+
+public class HackerRank015 {
+
+	// https://www.hackerrank.com/challenges/java-datatypes/problem
+	// https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+	//
+	// byte - 8-bit signed two's complement integer - -128 to 127
+	// short - 16-bit signed two's complement integer - -32,768 and 32,767
+	// ...
+	// int - 32-bit signed two's complement integer - -128 to 127
+	// byte - 64-bit signed two's complement integer - -128 to 127
+	//
+	//
+	public static void main(String[] args) {
+
+	}
+}
